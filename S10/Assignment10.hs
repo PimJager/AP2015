798b2df99d2f23c30a8e4f819e46003c1207d42d
pimjager <pim@pimjager.nl>
2015-12-07 13:42:56 +0100

    Assignment 11

--- S10/Assignment10.hs
+++ S10/Assignment10.hs
@@ -8,15 +8,22 @@ module Assignment10 where
 import Data.Functor
 import Control.Monad
 import Control.Monad.State
-import Data.Monoid
+import Data.Monoid hiding (Any, All)
 import Data.List.Split
+import qualified Data.List as L
 
 data Matcher a where
-    Is          :: (Matchable a) => (Matcher a) -> Matcher a
-    EqualTo     :: (Matchable a) => a -> Matcher a
-    LessThan    :: (Matchable a) => a -> Matcher a
-    Not         :: (Matchable a) => (Matcher a) -> Matcher a
-    Either      :: (Matchable a) => (Matcher a) -> (Matcher a) -> Matcher a
+    Is              :: (Matchable a) => (Matcher a) -> Matcher a
+    EqualTo         :: (Matchable a) => a -> Matcher a
+    LessThan        :: (Matchable a) => a -> Matcher a
+    Not             :: (Matchable a) => (Matcher a) -> Matcher a
+    Either          :: (Matchable a) => (Matcher a) -> (Matcher a) -> Matcher a
+    Contains        :: (Matchable a) => a -> Matcher [a]
+    Any             :: (Matchable a) => Matcher a -> Matcher [a] 
+        -- does the matcher hold for ANY value in the list?
+    All             :: (Matchable a) => Matcher a -> Matcher [a]
+        -- does the matcher hold for ALL values in the list
+    ContainsString  ::                  String -> Matcher String
 
 deriving instance Show (Matcher a)
 
@@ -25,6 +32,7 @@ class (Show a, Ord a, Eq a) => Matchable a
 deriving instance Matchable Bool
 deriving instance Matchable Int 
 deriving instance Matchable Char
+deriving instance (Matchable a) => Matchable [a] -- also covers String
 
 data AssertThat     = forall a. (Matchable a) => AssertThat String a (Matcher a)
                     |           Chain AssertThat AssertThat
@@ -32,6 +40,7 @@ data AssertThat     = forall a. (Matchable a) => AssertThat String a (Matcher a)
 
 -- in Haskell (*) is defined in the Num typeclass and can therefore not be overloaded 
 -- easily, so we use another approach 
+
 instance Monoid AssertThat where
     mempty          = Empty
     mappend Empty b = b
@@ -52,10 +61,14 @@ testAT (AssertThat d v m) = test' d v m
     test' _ v m         = testM v m
 
 testM :: (Matchable a) => a -> Matcher a -> Bool
-testM v (EqualTo x)  = v == x
-testM v (LessThan x) = v < x
-testM v (Not m)      = not $ testM v m
-testM v (Either x y) = testM v x || testM v y
+testM v (EqualTo x)         = v == x
+testM v (LessThan x)        = v < x
+testM v (Not m)             = not $ testM v m
+testM v (Either x y)        = testM v x || testM v y
+testM v (Contains x)        = elem x v
+testM v (Any m)             = any (\a -> testM a m) v
+testM v (All m)             = all (\a -> testM a m) v
+testM v (ContainsString s)  = L.isInfixOf s v
 
 -- deep embedding
 instance Show AssertThat where
@@ -89,6 +102,9 @@ correc2 = AssertThat "a == Either b a" 'a' $ Is $ Either (EqualTo 'b') (EqualTo 
 correI1 = AssertThat "1 /= 2" (1::Int) $ Not $ EqualTo 2
 fail1   = AssertThat "a == Either b c" 'a' $ Is $ Either (EqualTo 'b') (EqualTo 'c')
 fail2   = AssertThat "2 == Either 1 3" (2::Int) $ Is $ Either (EqualTo 1) (EqualTo 3)
+fail5   = AssertThat "all [1..4] EqualTo 2" [(1::Int)..4] $ All $ EqualTo 2
+correc5 = AssertThat "any [1..4] EqualTo 2" [(1::Int)..4] $ Any $ EqualTo 2
+correc6 = AssertThat "all [1..4] < 5" [(1::Int)..4] $ All $ LessThan 5
 
 mix     = correc1 .* correI1 .* fail1 .* correc2 .* fail2
 
@@ -98,17 +114,25 @@ mix     = correc1 .* correI1 .* fail1 .* correc2 .* fail2
 -- Note: If this would be a library and we wouldn't want our users to be
 -- able to user the deep constructors directly, it would simply be possible to
 -- only export the shallow functions
-is          :: (Matchable a) => (Matcher a) -> Matcher a
-is          = Is
-equalTo     :: (Matchable a) => a -> Matcher a
-equalTo     = EqualTo
-lessThan    :: (Matchable a) => a -> Matcher a
-lessThan    = LessThan
+is              :: (Matchable a) => (Matcher a) -> Matcher a
+is              = Is
+equalTo         :: (Matchable a) => a -> Matcher a
+equalTo         = EqualTo
+lessThan        :: (Matchable a) => a -> Matcher a
+lessThan        = LessThan
 --not and either are primed to not clash with Prelude
-not'         :: (Matchable a) => (Matcher a) -> Matcher a
-not'        = Not      
-either'      :: (Matchable a) => (Matcher a) -> (Matcher a) -> Matcher a
-either'     = Either
+not'            :: (Matchable a) => (Matcher a) -> Matcher a
+not'            = Not      
+either'         :: (Matchable a) => (Matcher a) -> (Matcher a) -> Matcher a
+either'         = Either
+contains        :: (Matchable a) => a -> Matcher [a]
+contains        = Contains
+containsString  :: String -> Matcher String
+containsString  = ContainsString
+any'            :: (Matchable a) => Matcher a -> Matcher [a] 
+any'            = Any
+all'            :: (Matchable a) => Matcher a -> Matcher [a]
+all'            = All
 
 assertThat :: forall a. (Matchable a) => String -> a -> (Matcher a) -> AssertThat
 assertThat = AssertThat
@@ -120,8 +144,28 @@ fail4   = assertThat "2 == either 3 9" (2::Int) $ either' (equalTo 3) (equalTo 9
 
 mix2    = correc3 .* fail3 .* fail4 .* correc4
 
+-- Test cases from assignment
 
+allA = a1 .* a2 .* a3 .* a4 .* a5 .* a6 .* a7 .* a8 .* a9
+a1 = AssertThat "a1" ((2::Int)*2) $ Is $ EqualTo $ 2+2
+a2 = AssertThat "a2" ((3::Int)*3) $ EqualTo $ 3+3
+a3 = AssertThat "a3" (length [0..3]) $ Not $ EqualTo 4
+a4 = assertThat "a4" [(0::Int)..3] $ contains 2
+a5 = AssertThat "a5" [(0::Int)..3] $ Contains 7
+a6 = AssertThat "a6" [(0::Int)..3] $ Either (EqualTo [1]) (Contains 7)
+a7 = AssertThat "a7" "hello world" $ ContainsString "hello"
+a8 = AssertThat "a8" "hello world" $ ContainsString "world"
+a9 = AssertThat "a9" "Who is afraid of red, yellow and blue" $ ContainsString "Red"
 
--- Voor list en andere shit
--- GADT, laat dan een matcher op een specifiek type uitkomen (bijv. Matcher [a])
--- 
+{- 
+output of executing > test allA
+
+*Assignment10 L> test allA
+passes = 4, fails = 5
+1: a2
+2: a3
+3: a5
+4: a6
+5: a9
+
+-}
